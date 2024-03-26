31a528be525abf40140713c1f8ac5972ca19f81d
Stephanie <Stephaniechloe21@gmail.com>
2024-03-26 19:35:14 +0100

    final fix

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -7,4 +7,4 @@ def schools_by_topic(mongo_collection, topic):
     Python function that changes all topics of a
     school document based on the name
     """
-    return list(mongo_collection.find_one({"topic": topic}))
+    return list(mongo_collection.find({"topics": topic}))
